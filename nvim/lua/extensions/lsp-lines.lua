36192bd16c744126a7d205d08ce7a8edf72e5a39
Julian Da Cunha <j.dacunha@pm.me>
2023-07-25 21:21:39 +0200

    Update Neotree config and bindings

--- nvim/lua/extensions/lsp-lines.lua
+++ nvim/lua/extensions/lsp-lines.lua
@@ -6,9 +6,68 @@
 
 local lsp_lines = require("lsp_lines")
 
+fn.sign_define(
+  "DiagnosticSignError",
+  {
+    text = "",
+    texthl = "DiagnosticSignError",
+    numhl = "",
+    linehl = "",
+  }
+)
+
+fn.sign_define(
+  "DiagnosticSignWarn",
+  {
+    text = "",
+    texthl = "DiagnosticSignWarn",
+    numhl = "",
+    linehl = "",
+  }
+)
+
+fn.sign_define(
+  "DiagnosticSignInfo",
+  {
+    text = "",
+    texthl = "DiagnosticSignInfo",
+    numhl = "",
+    linehl = "",
+  }
+)
+
+fn.sign_define(
+  "DiagnosticSignHint",
+  {
+    text = "",
+    texthl = "DiagnosticSignHint",
+    numhl = "",
+    linehl = "",
+  }
+)
+
+
 -- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config({
-    virtual_text = false,
+  virtual_text = false,
+  virtual_lines = false,
+  -- signs = false,
 })
 
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- delay update diagnostics
+    update_in_insert = true,
+  }
+)
+
+
 lsp_lines.setup()
+
+vim.keymap.set(
+  "",
+  "<leader>l",
+  lsp_lines.toggle,
+  { desc = "Toggle lsp_lines" }
+)
