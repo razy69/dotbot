62b5a93c04f5e4885b766b3989f28449edfde1d6
razy69 <j.dacunha@protonmail.com>
2025-03-04 09:38:45 +0100

    Cleanup autocmd nvim
    Signed-off-by: razy69 <razy69@protonmail.com>

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -6,48 +6,22 @@
 require "helpers/globals"
 
 
--- User [[[
-
 -- Copied from LazyVim
 local function augroup(name)
   return api.nvim_create_augroup("razyvim_" .. name, { clear = true })
 end
 
--- Python autocmd to setup tab
+-- Python setup tabs
 api.nvim_create_autocmd("FileType", {
   pattern = "python",
-  callback = function ()
+  callback = function()
     opt.tabstop = 4
     opt.softtabstop = 4
     opt.shiftwidth = 4
   end
 })
 
--- Check if we need to reload the file when it changed
-api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  group = augroup("checktime"),
-  command = "checktime",
-})
-
--- Highlight on yank
-api.nvim_create_autocmd("TextYankPost", {
-  group = augroup("highlight_yank"),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- resize splits if window got resized
-api.nvim_create_autocmd({ "VimResized" }, {
-  group = augroup("resize_splits"),
-  callback = function()
-    local current_tab = fn.tabpagenr()
-    cmd("tabdo wincmd =")
-    cmd("tabnext " .. current_tab)
-  end,
-})
-
--- go to last loc when opening a buffer
+-- Go to last loc when opening a buffer
 api.nvim_create_autocmd("BufReadPost", {
   group = augroup("last_loc"),
   callback = function(event)
@@ -65,7 +39,7 @@ api.nvim_create_autocmd("BufReadPost", {
   end,
 })
 
--- wrap and check for spell in text filetypes
+-- Wrap and check for spell in text filetypes
 api.nvim_create_autocmd("FileType", {
   group = augroup("wrap_spell"),
   pattern = { "gitcommit", "markdown" },
@@ -86,55 +60,18 @@ api.nvim_create_autocmd({ "BufWritePre" }, {
     fn.mkdir(fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
--- ]]]
 
-
--- alpha.vim [[[
-
--- Disable folding on alpha buffer
+-- Alpha vim Disable folding on alpha buffer
 api.nvim_create_autocmd("FileType", {
   pattern = "alpha",
-  callback = function ()
+  callback = function()
     opt.foldenable = false
   end
 })
 
--- Hide/Show tabline
-api.nvim_create_autocmd("User", {
-	pattern = "AlphaReady",
-	desc = "disable tabline for alpha",
-	callback = function()
-		opt.showtabline = 0
-		local hl = api.nvim_get_hl_by_name("Cursor", true)
-		hl.blend = 100
-		api.nvim_set_hl(0, "Cursor", hl)
-	  opt.guicursor:append("a:Cursor/lCursor")
-		require("illuminate").invisible_buf()
-	end,
-})
-
-api.nvim_create_autocmd("BufUnload", {
-	buffer = 0,
-	desc = "enable tabline after alpha",
-	callback = function()
-		opt.showtabline = 2
-		local hl = api.nvim_get_hl_by_name("Cursor", true)
-		hl.blend = 0
-		api.nvim_set_hl(0, "Cursor", hl)
-		opt.guicursor:remove("a:Cursor/lCursor")
-	end,
-})
-
--- ]]]
-
-
--- lint.nvim [[[
-
 -- Lint on Leave Insert Mode
-api.nvim_create_autocmd("BufWritePost" , {
+api.nvim_create_autocmd("BufWritePost", {
   callback = function()
     require("lint").try_lint()
   end
 })
-
--- ]]]
