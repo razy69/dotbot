cfd6af8b2957d2ceff021964c94a3b88c505c225
razy69 <j.dacunha@protonmail.com>
2025-03-04 09:39:20 +0100

    fix nvim-cmp + add zsh uuid_gen func

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -270,7 +270,7 @@ return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = {
-      enable_check_bracket_line = true,
+      enable_check_bracket_line = false,
       check_ts = true,
     }
   },
