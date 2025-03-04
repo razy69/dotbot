17638cdb9bf6eae9bccbb8f523eecc7abffcd117
razy69 <j.dacunha@protonmail.com>
2025-03-04 09:38:11 +0100

    improve lsp and cmp configs

--- nvim/lua/extensions/cmp.lua
+++ nvim/lua/extensions/cmp.lua
@@ -5,9 +5,20 @@
 ]]
 
 
-local cmp = require("cmp")
 local lspkind = require("lspkind")
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
+
+lspkind.init{
+  mode = "symbol_text",
+  preset = "codicons",
+}
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
 
@@ -23,7 +34,7 @@ cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+	    luasnip.lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
 
@@ -33,33 +44,66 @@ cmp.setup({
 		-- Autocompletion menu
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping({
-			i = function(fallback)
-				if cmp.visible() and cmp.get_active_entry() then
-					cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-				else
-					fallback()
-				end
-			end,
-			s = cmp.mapping.confirm({ select = true }),
-			c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-		}),
+		["<CR>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+        fallback()
+      elseif cmp.visible() and cmp.get_active_entry() then
+        cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 
 		-- Use <C-p> and <C-n> to navigate through completion variants
-		["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-		["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-		["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-		["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+		["<C-n>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+       	cmp.complete()
+      else
+        fallback()
+      end
+		end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+       	cmp.complete()
+      else
+        fallback()
+      end
+		end, { "i", "s" }),
+
+    ["<C-p>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+      	cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+      	cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 	}),
 
+	autocomplete = false,
+
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },                -- LSP
-		{ name = "nvim_lsp_signature_help" }, -- LSP for parameters in functions
-		{ name = "buffer" },                  -- Buffers
-		{ name = "luasnip" },                 -- Snippets
-		{ name = "path" },                    -- Paths
-		{ name = "nvim_lua" },                -- Lua Neovim API
-		{ name = "emoji" },                   -- Emoji
+    { name = "nvim_lsp" },
+    { name = "nvim_lsp_signature_help" },
+    { name = "treesitter", keyword_length = 3, max_item_count = 3 },
+    { name = "path" },
+    { name = "buffer", keyword_length = 3, max_item_count = 3 },
+    { name = "luasnip", keyword_length = 3, max_item_count = 3 },
+    { name = "pandoc_references" },
+    { name = "spell" },
+    { name = "calc" },
+    { name = "emoji" },
 	}),
 
 	sorting = {
@@ -77,10 +121,11 @@ cmp.setup({
 
 	formatting = {
 		format = lspkind.cmp_format({
-			mode = "symbol", -- show only symbol annotations
 			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 		})
 	},
 
 })
+
+require("luasnip.loaders.from_vscode").lazy_load()
