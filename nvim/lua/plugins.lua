13563eac47ed13f10a7964119aff5d2f2d6a3899
razy69 <j.dacunha@protonmail.com>
2025-03-04 09:38:38 +0100

    Fixed: Some term draw issues
    Signed-off-by: razy69 <razy69@protonmail.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -208,7 +208,7 @@ return {
   {
     "nvim-telescope/telescope-fzf-native.nvim",
     build =
-    "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    "cmake -S . -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
   },
   {
     "nvim-telescope/telescope.nvim",
